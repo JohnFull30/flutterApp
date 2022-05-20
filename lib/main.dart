8a83d34344ea8a713395390aa3fe6eaa4f705984
JohnFull30 <johnathanfuller0@gmail.com>
2022-05-19 23:56:47 -0400

    learned how touse the padding and margin as well as EdgeInsets

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,15 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      body: Center(
-        child: Image.asset('assets/F.png')
-      ),
+      body: Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(20.0),
+            margin: EdgeInsets.all(30.0),
+            child: Text('I am in a box'),
+          ),
+      // body: Center(
+      //     child: Image.asset('assets/F.png')
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
         child: Text("Click"),
